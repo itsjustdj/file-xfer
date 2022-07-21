69e13b5c5d391e9fb2e94e52d73c3e6e276194bf
D.J Billings <dj@itsjustdj.com>
2022-07-21 15:53:59 -0700

    Add script file

---
+++ xfer-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A script to move music files to local server via ssh
+
+DESTDIR=[server-directory-path]
+
+echo -e "Which music folder do you want to transfer?"
+read directory
+
+#The quotes around the directory variable below are to allow for spaces in directory names.
+
+scp -r "$directory" $DESTDIR
+
+echo "Your music has been transferred successfully! Or not, you may want to check."
